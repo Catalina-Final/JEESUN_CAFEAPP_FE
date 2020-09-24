9658106a6c73d8a36436177c25ad35d03a70a196
jeesun <luckymeday@gmail.com>
2020-09-24 18:45:05 +0700

    register fixed

--- src/containers/RegisterPage/index.js
+++ src/containers/RegisterPage/index.js
@@ -59,99 +59,108 @@ const RegisterPage = () => {
   }, [dispatch, history, redirectTo]);
 
   return (
-    <Container>
-      <Row>
-        <Col md={{ span: 6, offset: 3 }}>
-          <div className="text-center mb-3">
-            <h1 className="text-primary">Sign Up</h1>
-            <p className="lead"> Create Your Account</p>
-          </div>
+    <div className="main-container montserrat">
+      <Container>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <div className="text-center mb-3">
+              <h1 className="point">Sign Up</h1>
+              <p className="lead label"> Create Your Account</p>
+            </div>
 
-          <Form onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                required
-                placeholder="Name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-              />
-              {errors.name && (
-                <small className="form-text text-danger">{errors.name}</small>
-              )}
-              {/* 동일한 내용 -> {errors.name ? (
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  required
+                  placeholder="Name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+                {errors.name && (
+                  <small className="form-text text-danger">{errors.name}</small>
+                )}
+                {/* 동일한 내용 -> {errors.name ? (
                 <small className="form-text text-danger">{errors.name}</small>
               ) : null} */}
-            </Form.Group>
+              </Form.Group>
 
-            <Form.Group>
-              <Form.Control
-                type="email"
-                required
-                placeholder="Email Address"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-              />
-              {errors.email && (
-                <small className="form-text text-danger">{errors.email}</small>
-              )}
-              {/* 동일한 내용 -> {errors.email ? (
+              <Form.Group>
+                <Form.Control
+                  type="email"
+                  required
+                  placeholder="Email Address"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                {errors.email && (
+                  <small className="form-text text-danger">
+                    {errors.email}
+                  </small>
+                )}
+                {/* 동일한 내용 -> {errors.email ? (
                 <small className="form-text text-danger">{errors.email}</small>
               ) : null} */}
-            </Form.Group>
+              </Form.Group>
 
-            <Form.Group>
-              <Form.Control
-                type="password"
-                required
-                placeholder="Password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-              />
-              {errors.password && (
-                <small className="form-text text-danger">
-                  {errors.password}
-                </small>
-              )}
-            </Form.Group>
-            <Form.Group>
-              <Form.Control
-                type="password"
-                placeholder="Confirm Password"
-                name="password2"
-                value={formData.password2}
-                onChange={handleChange}
-              />
-              {/* {errors.password2 && (
+              <Form.Group>
+                <Form.Control
+                  type="password"
+                  required
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+                {errors.password && (
+                  <small className="form-text text-danger">
+                    {errors.password}
+                  </small>
+                )}
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="password2"
+                  value={formData.password2}
+                  onChange={handleChange}
+                />
+                {/* {errors.password2 && (
                 <small className="form-text text-danger">
                   {errors.password2}
                 </small>
               )} -> 이미 암호1에 에러메세지가 있긴 때문에 굳이 쓰지 않아도 됨 */}
-            </Form.Group>
+              </Form.Group>
 
-            {loading ? (
-              <Button
-                className="btn-block"
-                variant="primary"
-                type="button"
-                disabled
-              >
-                <span
-                  className="spinner-border spinner-border-sm"
-                  role="status"
-                  aria-hidden="true"
-                ></span>{" "}
-                Loading...
-              </Button>
-            ) : (
-              <Button className="btn-block" type="submit" variant="primary">
-                Register
-              </Button>
-            )}
-            {/* <Button
+              {loading ? (
+                <Button
+                  className="btn-block"
+                  variant="primary"
+                  type="button"
+                  style={{ backgroundColor: "#f57f5b", border: "none" }}
+                  disabled
+                >
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    role="status"
+                    aria-hidden="true"
+                  ></span>{" "}
+                  Loading...
+                </Button>
+              ) : (
+                <Button
+                  className="btn-block"
+                  type="submit"
+                  variant="primary"
+                  style={{ backgroundColor: "#f57f5b", border: "none" }}
+                >
+                  Register
+                </Button>
+              )}
+              {/* <Button
               className="btn-block"
               type="button"
               variant="light"
@@ -159,13 +168,17 @@ const RegisterPage = () => {
             >
               Fill in fake data
             </Button> */}
-            <p>
-              Already have an account? <Link to="/login">Sign In</Link>
-            </p>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+              <p>
+                Already have an account?{" "}
+                <Link to="/login" className="point">
+                  Sign In
+                </Link>
+              </p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
